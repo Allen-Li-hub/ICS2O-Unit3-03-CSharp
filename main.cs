111cecdcf0cb8736dd3589fccea4ece79b4a521f
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-06 19:33:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: Allen
+// Created on: Oct 2022
+//
+// This program calculates Volume of Sphere
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        float radiusOfSphere;
+        float volumeOfSphere;
+
+        Console.WriteLine("This program calculates the area of a triangle.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the radius (cm): ");
+        radiusOfSphere = Convert.ToInt32(Console.ReadLine());
+
+        volumeOfSphere = (float)(4.0 / 3 * Math.PI * radiusOfSphere * radiusOfSphere*radiusOfSphere);
+
+        Console.WriteLine("");
+        Console.WriteLine("The volume is: " + volumeOfSphere.ToString("0.00") + " cm³. ");
+
+        Console.WriteLine("\nDone.");
+    }
+}
